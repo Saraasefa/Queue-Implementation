56edc8810b09e3b3a1c3b2db72d8fa921af84387
G2 SOFWARE ENGINEERING SECTION B GROUP 5 <kingoebisa@gmail.com>
2024-11-09 06:53:10 -1200

    Update queue-using-array.cpp

--- queue-using-array.cpp
+++ queue-using-array.cpp
@@ -6,13 +6,7 @@ int A[SIZE];
 int front = -1;
 int rear = -1;
 //function to check if queue is empty
-bool isempty()
-{
- if(front == -1 && rear == -1)
- return true;
- else
- return false;
-}
+
 //function to insert element in queue
 void enqueue ( int value )
 {
