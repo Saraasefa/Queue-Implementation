cc775008924e67eb685cbb78662651226568b592
Saraasefa <sasefa535@gmail.com>
2024-11-09 22:00:23 +0300

    Update queue-using-array.cpp

--- queue-using-array.cpp
+++ queue-using-array.cpp
@@ -6,7 +6,13 @@ int A[SIZE];
 int front = -1;
 int rear = -1;
 //function to check if queue is empty
-
+bool isempty()
+{
+ if(front == -1 && rear == -1)
+ return true;
+ else
+ return false;
+}
 //function to insert element in queue
 void enqueue ( int value )
 {
